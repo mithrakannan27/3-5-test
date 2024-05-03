4cab29ca6555192bed1c180c3ef78774c6698465
mithrakannan27 <164007915+mithrakannan27@users.noreply.github.com>
2024-05-03 12:54:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const one=document.getElementById("one");
+const two=document.getElementById("two");
+const three=document.getElementById("three");
+const four=document.getElementById("four");
+const five=document.getElementById("five");
+
+one.onclick=function(){
+    one.style.color="white";
+    one.style.backgroundColor="hsl(217, 12%, 63%)";
+}
+
+two.onclick=function(){
+    two.style.color="white";
+    two.style.backgroundColor="hsl(217, 12%, 63%)";
+}
+
+three.onclick=function(){
+    three.style.color="white";
+    three.style.backgroundColor="hsl(217, 12%, 63%)";
+}
+
+four.onclick=function(){
+    four.style.color="white";
+    four.style.backgroundColor="hsl(217, 12%, 63%)";
+}
+
+five.onclick=function(){
+    five.style.color="white";
+    five.style.backgroundColor="hsl(217, 12%, 63%)";
+}
+
